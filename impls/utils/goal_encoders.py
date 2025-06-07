Add log_std clipping to VIB to fix numerical instability issues

impls/utils/goal_encoders.py
@@ -6,17 +6,15 @@ class VIBEncoder(nn.Module):
     encoder: nn.Module
     beta: int = 1.0
     rep_dim: int = 256
+    log_std_min: int = -5
 
     @nn.compact
     def __call__(self, goal, rng):
         goal = self.encoder(goal)
-        mean, sd = nn.Dense(self.rep_dim)(goal), nn.Dense(self.rep_dim)(goal)
-        sd = nn.softplus(sd - 5.0)
+        mean, log_stds = nn.Dense(self.rep_dim)(goal), nn.Dense(self.rep_dim)(goal)
+        log_stds = jnp.clip(log_stds, min=self.log_std_min)
 
-        # # reparameterization trick
-        # eps = jax.random.normal(new_rng, sd.shape)
-        # z = eps * sd + mean
-        dist = distrax.MultivariateNormalDiag(loc=mean, scale_diag=sd)
+        dist = distrax.MultivariateNormalDiag(loc=mean, scale_diag=jnp.exp(log_stds))
         prior = distrax.MultivariateNormalDiag(loc=jnp.zeros_like(mean))
         z, log_probs = dist.sample_and_log_prob(seed=rng)
         kl_loss = log_probs - prior.log_prob(z)